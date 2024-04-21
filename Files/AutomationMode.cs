32e5a15cdd2905175850ca34b55c19c21371a1b9
adsamcik <adsamcik@users.noreply.github.com>
2024-04-21 12:45:14 +0200

    Create AutomationMode.cs

---
+++ Files/AutomationMode.cs
@@ -0,0 +1,24 @@
+﻿namespace PhotoCopy.Files
+{
+    /// <summary>
+    /// Defines the modes of automation for handling certain operations.
+    /// </summary>
+    internal enum AutomationMode
+    {
+        /// <summary>
+        /// The operation will prompt the user for input or decision.
+        /// </summary>
+        Prompt,
+
+        /// <summary>
+        /// The operation will abort if an issue arises.
+        /// </summary>
+        Abort,
+
+        /// <summary>
+        /// The operation will attempt to automatically resolve any issues.
+        /// </summary>
+        Automate
+    }
+
+}
